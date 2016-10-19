2dfb76f5caf2200f356634f375cbfb54af80d4b2
jon <jonathan.lazarini@wipro.com>
2016-10-19 20:17:10 +0100

    FIX import for js and css with jade

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,5 @@
+//TODO Add preinstall hook to set up ruby and sass gem, "preinstall": ""
+
 var write = require('./write');
 window.addEventListener('DOMContentLoaded', write);
 console.log('hello world')
