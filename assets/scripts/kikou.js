99eb35bfd28671012fb33e17760648373a05931f
jon <jonathan.lazarini@wipro.com>
2016-10-20 17:07:50 +0100

    clean

--- assets/scripts/kikou.js
+++ assets/scripts/kikou.js
@@ -2,5 +2,5 @@
  * Created by jonlazarini on 19/10/16.
  */
 
-const kikou = console.log("kikou")
+const kikou = "kikou"
 export default kikou
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,5 @@
 //TODO Add preinstall hook to set up ruby and sass gem, "preinstall": ""
+//TODO instead of hard watch changes i watchify and pipe it in the workflow
 
 var write = require('./write');
 window.addEventListener('DOMContentLoaded', write);
@@ -6,3 +7,4 @@ console.log('hello world')
 
 import kikou from './kikou'
 console.log(kikou)
+
