b81fb8564c3de7b3eb73ea44c3025d6ff67876ef
jon <jonathan.lazarini@wipro.com>
2016-10-26 13:10:01 +0100

    clean src and markup,styles dir, create layout,includes for pug files

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,11 @@
 //TODO Add preinstall hook to set up ruby and sass gem, "preinstall": ""
-//TODO instead of hard watch changes i watchify and pipe it in the workflow
 
-var write = require('./write');
-window.addEventListener('DOMContentLoaded', write);
+// var write = require('./write');
+// window.addEventListener('DOMContentLoaded', write);
 console.log('hello world')
 
 import kikou from './kikou'
 console.log(kikou)
 
-import { api as ap} from './semantic-ui/api'
-console.log('before import')
-console.log(ap)
-console.log('after import')
+// import semanticUi from './semantic-ui'
+// console.log(semanticUi)
