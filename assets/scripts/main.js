3cb24af484cddecb7ab19c1ab9c47c70d3735087
jon <jonathan.lazarini@wipro.com>
2016-10-27 17:31:18 +0100

    clean index and change build script

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,5 @@
 //TODO Add preinstall hook to set up ruby and sass gem, "preinstall": ""
 import accordion from './semantic-ui/accordion'
-// var write = require('./write');
-// window.addEventListener('DOMContentLoaded', write);
 /**
  * jQuery is a global which semantic-ui expects to receive as jQuery (the
  * global)
@@ -10,8 +8,8 @@ import accordion from './semantic-ui/accordion'
 * */
 //TODO add rule for global in eslint as it will crash as linting does not
   // expect globals in modules...
-
 var $ = jQuery = window.$ = window.jQuery
 
+window.global = "global"
 // **** Beginning code ****
 $('.ui.accordion').accordion();
