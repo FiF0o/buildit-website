c1ab4d84e3e507dca552a075abe66afe06692bc5
jon <jonathan.lazarini@wipro.com>
2016-10-27 12:32:38 +0100

    FIX issue with instantiating semantic-ui js plugins, needs to use the global jQuery and not jQuery modules

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,21 +1,17 @@
 //TODO Add preinstall hook to set up ruby and sass gem, "preinstall": ""
-import kikou from './kikou'
-import $ from 'jquery'
 import accordion from './semantic-ui/accordion'
-
-
-
 // var write = require('./write');
 // window.addEventListener('DOMContentLoaded', write);
 
-console.log(kikou)
+/**
+ * jQuery is a global which semantic-ui expects to receive as jQuery (the
+ * global)
+ * reassigns global jQuery as a local var $
+* */
+//TODO add rule for global in eslint as it will crash as linting does not
+  // expect globals in modules...
 
-// window.$ = window.jQuery = jQuery
-// global.jQuery = require('jquery');
-console.log('$: ',$)
-console.log('accordion: ',accordion)
-// console.log(accordion)
+var $ = jQuery
 
-$('.ui.accordion')
-  .accordion()
-;
+// **** Beginning code ****
+$('.ui.accordion').accordion();
