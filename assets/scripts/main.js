3b3cd013a86ae0f1a6e10277ac4f7c4f2cace7e4
jon <jonathan.lazarini@wipro.com>
2016-11-03 21:52:27 +0000

    clean pug, fix styling on core components, add lazy-loading images with dynamic list, add sidebar menu behaviour

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,8 @@
 //TODO Add preinstall hook to set up ruby and sass gem, "preinstall": ""
 //import accordion from './semantic-ui/accordion'
 import visibility from './semantic-ui/visibility'
+import sticky from './semantic-ui/sticky'
+import sidebar from './semantic-ui/sidebar'
 
 /**
  * jQuery is a global which is augmented by semantic-ui library and expects to
@@ -15,57 +17,58 @@ var $ = jQuery = window.$ = window.jQuery
 
 window.global = "global"
 // **** Beginning code ****
-// debug test init
 
-$('.masthead')
-  .visibility({
-    once: false,
-    onBottomPassed: function() {
-      $('.fixed.menu').transition('fade in');
-    },
-    onBottomPassedReverse: function() {
-      $('.fixed.menu').transition('fade out');
-    }
-  });
-$('.masthead').visibility('refresh')
 
-// create sidebar and attach to menu open
-$('.ui.sidebar')
-  .sidebar('attach events', '.toc.item')
-;
-
-var arImg = ['images/avatar/stevie.jpg', 'images/avatar/toto.jpg', 'images/avatar/stevie3.jpg']
-
-var getImg = function(){
-  // adds a custom data-attribute to the markups with each()
-  $('img.transition.visible').each( function( index, value ) {
-    $(value).attr('data-test', arImg[index]);
-  });
-
-}
-window.getImg = getImg
-// adds image - should be replaced with appropriate image
-$('img.transition.visible').attr('data-src', 'images/avatar/stevie.jpg')
-                           .visibility({
-                             once: false,
-                             observeChanges: true,
-                             onTopVisible :function(calcs) {
-                               window.getImg()
-                               window.showContent()
-                             }
-                           })
-// $('.demo.items .image img')
+// $('.masthead')
 //   .visibility({
 //     once: false,
-//     // observeChanges: true,
-//     // onTopPassed: function() {
-//     //   var q = $('.item.image')
-//     //   console.log(q)
-//     //   //data-src="images/avatar/stevie.jpg
-//     type       : 'image',
-//     transition : 'fade in',
-//     duration   : 3000
+//     onBottomPassed: function() {
+//       $('.fixed.menu').transition('fade in')
+//     },
+//     onBottomPassedReverse: function() {
+//       $('.fixed.menu').transition('fade out')
+//     }
 //   });
+// $('.masthead').visibility('refresh')
+
+
+// attaches toggle event of sidebar to .toc.item to menu open
+// $('.ui.sidebar')
+  // .sidebar('attach events', '.toc.item')
+  // .sidebar({
+  //   // context in which the sidebar will appear
+  //   context: $('.pusher'),
+  // })
+
+
+// init sidebar behavior
+$('.toc.item').click(function() {
+  $('.ui.sidebar')
+    .sidebar('toggle');
+})
+
+// init sticky content
+$('.ui.sticky')
+  .sticky({
+    // context: '#sticky-content'
+  })
+
+// init images lazy-loading
+$('img.transition.visible')
+ .visibility({
+   once: false,
+   type: 'images',
+   observeChanges: true,
+   transition: 'fade in',
+   duration: ' 1000',
+   onTopVisible :function(calcs) {
+     // lazy-loading - adds data-src attribute to the img tag
+     window.getImg()
+     window.showContent()
+   }
+ })
+
+
 var showContent = function () {
   setTimeout(function() {
     $('.demo.items .image img')
@@ -73,10 +76,21 @@ var showContent = function () {
         once: false,
         type       : 'image',
         transition : 'fade in',
-        duration   : 3000
+        duration   : 1000
       });
     // needed by semantic-ui when using visibility() otherwise throws an error
     $('.demo.items .image img').visibility('refresh')
-  }, 1000)
+  }, 500)
 }
 window.showContent = showContent
+
+
+var arImg = ['images/avatar/stevie.jpg', 'images/avatar/nan.jpg', 'images/avatar/tom.jpg']
+var getImg = function(){
+  // adds a custom data-attribute to the markups with each()
+  return $('img.transition.visible').each( function( index, value ) {
+    $(value).attr('data-test', arImg[index])
+  })
+
+}
+window.getImg = getImg
